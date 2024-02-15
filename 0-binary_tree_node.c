a4762c36587ce1c7ccdc49629100344aad34dbcb
muzijerrymuller <jerrymuller883@gmail.com>
2024-02-15 08:25:02 -0800

    nodes tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,20 +8,18 @@
  * Return: If error happens then NULL.
  * else - Pointer to the new node.
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node;
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL)
-{
-return (NULL);
-}
+    if (new_node != NULL)
+    {
+        new_node->n = value;
+        new_node->parent = parent;
+        new_node->left = NULL;
+        new_node->right = NULL;
+    }
 
-new_node->n = value;
-new_node->parent = parent;
-new_node->left = NULL;
-new_node->right = NULL;
-
-return (new_node);
+    return new_node;
 }
