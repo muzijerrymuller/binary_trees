33135dca6e4216a8d8446bc3f5364ef477fa210b
muzijerrymuller <jerrymuller883@gmail.com>
2024-02-23 03:07:39 -0800

    .....

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,14 +10,3 @@
  * else - Pointer to the created node.
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-{
-	binary_tree_t *lumina = (binary_tree_t *)malloc(sizeof(*lumina));
-
-	if (!lumina)
-	return NULL;
-
-	*lumina = (binary_tree_t){.parent = parent, .value = value, .left = NULL, .right = NULL};
-
-	return lumina;
-}
