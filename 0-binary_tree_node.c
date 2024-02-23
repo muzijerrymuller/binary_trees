cbfb780b7f844cbe362f45f57ce2e9403508610a
muzijerrymuller <jerrymuller883@gmail.com>
2024-02-23 01:33:08 -0800

    ternary

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,19 +10,11 @@
  * else - Pointer to the created node.
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 {
-	binary_tree_t *lumina;
+	binary_tree_t *lumina = (binary_tree_t *)malloc(sizeof(*lumina));
+	if (!lumina) return NULL;
 
-	lumina = (binary_tree_t *)malloc(sizeof(*lumina));
-
-	if (!lumina)
-	return NULL;
-
-	lumina->parent = parent;
-	lumina->value = value;
-	lumina->left = NULL;
-	lumina->right = NULL;
-
+	lumina->parent = parent, lumina->value = value, lumina->left = NULL, lumina->right = NULL;
 	return lumina;
 }
