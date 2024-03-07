d873282272a4ff5df0e41b4b7d72173f7b10625e
muzijerrymuller <jerrymuller883@gmail.com>
2024-03-07 05:41:04 -0800

    leaf

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,8 +3,6 @@
 /**
  * binary_tree_leaves - Checks how many leaves there rae in a binary tree.
  * @tree: Pointer to count how many leaves there are in a tree.
- *
- * Return: Number of leaves.
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
@@ -16,5 +14,6 @@ size_t leaves = 0, left = 0, right = 0;
 
 left = binary_tree_leaves(tree->left);
 right = binary_tree_leaves(tree->right);
+
 return ((!left && !right) ? leaves + 1 : leaves + 0);
 }
