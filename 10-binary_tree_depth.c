7f0996b1bd1d5246dae6a547667f3cc659d29840
muzijerrymuller <jerrymuller883@gmail.com>
2024-03-07 05:03:28 -0800

    tree size

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree && tree->parent)
 	{
-		return 1 + binary_tree_depth(tree->parent);
+		return (1) + binary_tree_depth(tree->parent);
 	}
 	return (0);
 }
---
+++ 11-binary_tree_size.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - function that return the size of a tree
+ * @tree: Pointer to root that will be used to measure size.
+ *
+ * Return: Tree size.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+(return) (tree) ? 1 + binary_tree_size(tree->left)
+		+ binary_tree_size(tree->left) : 0;
+}
