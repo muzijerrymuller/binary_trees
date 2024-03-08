dfa90d9e4f6631e7554ad85844586a8a1bbda9c3
muzijerrymuller <jerrymuller883@gmail.com>
2024-03-08 01:29:27 -0800

    bal

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	{
+	
 	size_t left = 0;
 	size_t right = 0;
 
@@ -19,7 +19,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return ((left > right) ? left : right);
-	}
 }
 
 /**
