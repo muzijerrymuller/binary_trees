f46b37bd13a306c48e5fa3d445f5ad755f297255
muzijerrymuller <jerrymuller883@gmail.com>
2024-03-08 01:33:37 -0800

    tre bal

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,18 +9,23 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	
 	size_t left = 0;
 	size_t right = 0;
 
-	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return ((left > right) ? left : right);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+			right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		}
+		return ((left > right) ? left : right);
+	}
 }
-
 /**
  * binary_tree_balance - Measures balance factor of a binary tree.
  * @tree: Pointer to the element that measures balance factor.
