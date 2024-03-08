b73f9a8d76ce306b782517d41eaaf114d1b7b2cd
muzijerrymuller <jerrymuller883@gmail.com>
2024-03-08 05:54:29 -0800

    binary tree full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,34 +8,34 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int l = 0, r = 0;
+int l = 0, r = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else if (tree->left == NULL && tree->right == NULL)
-		return (1)
-	
-	else			
-	{
-		if (tree->left && tree->right)
-		{
-			l = binary_tree_is_full(tree->left);
-			r = binary_tree_is_full(tree->right);
-			if (l == 0 ||  == 0)
-			{
-				return (0);
-			}
-			return (1);
-		}
-		else if (!tree->left && !tree->right) 
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
+if (tree == NULL)
+{
+return (0);
+}
+else if (tree->left == NULL && tree->right == NULL)
+return (1);
+
+else
+{
+if (tree->left && tree->right)
+{
+l = binary_tree_is_full(tree->left);
+r = binary_tree_is_full(tree->right);
+if (l == 0 || r == 0)
+{
+return (0);
+}
+return (1);
+}
+else if (!tree->left && !tree->right)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
 }
